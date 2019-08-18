c8e4f9c706dcaebcb77968f90187a7692b8ed65b
Matt Thornberry <mtberry16@hotmail.com>
2019-08-18 16:50:43 -0600

    Add function stubs

--- main.js
+++ main.js
@@ -1,10 +1,31 @@
-// document ready jQuery
 // User enters search area
-// User selects activity type
+function searchArea() {}
+
+// Fetch Google Maps data
+function fetchGoogleMapsData() {}
+
+// User selects hiking activity type
+function fetchHikingData() {}
+
 // User selects destination
-// User hit back button to select a different activity
+function selectDestination() {}
+
 // User wants to search a new area
-// User wants to go back to activity choices
-// DOM changes for search area entry
+function restartSearch() {}
+
 // Dom Changes for activity selection
+function addDestinationCards() {}
+
+// User selects back button to select a different activity
+function backToActivities() {}
+
 // Dom changes for destination selection
+function addDestinationDetails() {}
+
+//User selects back button to select a different destination
+function selectNewDestination() {}
+
+// document ready
+$(document).ready(function() {
+  console.log("Explorer ready, adventure awaits!");
+});
