5bccabf46de807b0c34e44e9430b075ded0d3e0d
Matt Thornberry <mtberry16@hotmail.com>
2019-08-18 07:06:38 -0600

    Add Google Icon Materials, Start user stories

---
+++ main.js
@@ -0,0 +1,10 @@
+// document ready jQuery
+// User enters search area
+// User selects activity type
+// User selects destination
+// User hit back button to select a different activity
+// User wants to search a new area
+// User wants to go back to activity choices
+// DOM changes for search area entry
+// Dom Changes for activity selection
+// Dom changes for destination selection
