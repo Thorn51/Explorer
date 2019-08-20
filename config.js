b1614e12c5352f2c6f76a27a75db53b74ed601a5
Matt Thornberry <34717832+Thorn51@users.noreply.github.com>
2019-08-20 15:11:45 -0600

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-let googleApiKey = {
-  key: "AIzaSyDFWfjid_23wFwEY1SVvBo5WTt6D_Acyog",
-  scriptSrc:
-    "https://maps.googleapis.com/maps/api/js?callback=initMap&libraries=places&key=AIzaSyDFWfjid_23wFwEY1SVvBo5WTt6D_Acyog"
-};
-
-let scriptSrc = googleApiKey.scriptSrc;
