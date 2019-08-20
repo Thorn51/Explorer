f860744bfbf7a2cd3d913c30bdd98f5924552c6c
Matt Thornberry <mtberry16@hotmail.com>
2019-08-20 09:12:54 -0600

    Test Google Maps Searchbox for Places

--- main.js
+++ main.js
@@ -1,31 +1,73 @@
 // User enters search area
-function searchArea() {
-  $(".search-area-button").on("click", function() {
-    event.preventDefault();
-    let $searchArea = $("#js-area-search").val();
-    if ($searchArea.length == 0) {
-      $(".invalid-entry").show();
-    } else {
-      $(".invalid-entry").hide();
-      $(".activities-container").show();
-      $([document.documentElement, document.body]).animate(
-        {
-          scrollTop: $("#activity-scroll").offset().top
-        },
-        1000
-      );
-      fetchGoogleMapsData($searchArea);
-    }
-  });
-}
 
 // Initialize Google Maps
-let map;
-
 function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 39.5501, lng: -105.7821 },
-    zoom: 5
+  var map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 37.0902, lng: -95.7129 },
+    zoom: 4,
+    disableDefaultUI: true,
+    mapTypeId: "roadmap"
+  });
+
+  // Create the search box and link it to the UI element.
+  var input = document.getElementById("pac-input");
+  var searchBox = new google.maps.places.SearchBox(input);
+  // map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+  // Bias the SearchBox results towards current map's viewport.
+  map.addListener("bounds_changed", function() {
+    searchBox.setBounds(map.getBounds());
+  });
+
+  var markers = [];
+  // Listen for the event fired when the user selects a prediction and retrieve
+  // more details for that place.
+  searchBox.addListener("places_changed", function() {
+    var places = searchBox.getPlaces();
+
+    if (places.length == 0) {
+      return;
+    }
+
+    // Clear out the old markers.
+    markers.forEach(function(marker) {
+      marker.setMap(null);
+    });
+    markers = [];
+
+    // For each place, get the icon, name and location.
+    var bounds = new google.maps.LatLngBounds();
+    places.forEach(function(place) {
+      if (!place.geometry) {
+        console.log("Returned place contains no geometry");
+        return;
+      }
+      var icon = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25)
+      };
+
+      // Create a marker for each place.
+      markers.push(
+        new google.maps.Marker({
+          map: map,
+          icon: icon,
+          title: place.name,
+          position: place.geometry.location
+        })
+      );
+
+      if (place.geometry.viewport) {
+        // Only geocodes have viewport.
+        bounds.union(place.geometry.viewport);
+      } else {
+        bounds.extend(place.geometry.location);
+      }
+    });
+    map.fitBounds(bounds);
   });
 }
 
@@ -49,6 +91,13 @@ function initMap() {
 //   let endPoint = formatQueryParams(mapParams);
 //   let searchUrl = baseUrl + endPoint;
 //   console.log(searchUrl);
+//   fetch(searchUrl)
+//     .then(function(response) {
+//       return response.json();
+//     })
+//     .then(function(data) {
+//       console.log(data);
+//     });
 // }
 
 // User selects hiking activity type
@@ -75,5 +124,4 @@ function selectNewDestination() {}
 // document ready
 $(document).ready(function() {
   console.log("Explorer ready, adventure awaits!");
-  searchArea();
 });
